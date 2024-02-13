7c3605b37b5e61320e963241f40da74a968524ef
Jason Liu <jliu@atomicsoftware.com>
2024-02-12 22:46:53 -0500

    add more configuration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from '@jasobs10/prisma';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,40 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiEnvironment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  const configService: ConfigService<ApiEnvironment, true> = app.get(ConfigService);
+
+  app.use(helmet());
+
+  app.setGlobalPrefix('api');
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    credentials: true,
+    origin: [configService.get('webBaseUrl')]
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API Docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
+  await app.listen(3000);
 }
 
 bootstrap();
